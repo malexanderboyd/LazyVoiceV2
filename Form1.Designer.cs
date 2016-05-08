a57c8d6e5556c52b7c332fc8531c61e384c6a8cb
Alex Boyd <m.alexander.boyd@gmail.com>
2016-05-08 17:41:48 -0400

    LazyVoice v1
    Basic Speech Recognition Program to handle volume up/down when sleeping

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LazyVoiceV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.enableVoiceButton = new System.Windows.Forms.Button();
+            this.disableVoiceButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(457, 246);
+            this.outputTextBox.TabIndex = 0;
+            this.outputTextBox.Text = "-Log-";
+            // 
+            // enableVoiceButton
+            // 
+            this.enableVoiceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enableVoiceButton.Location = new System.Drawing.Point(12, 291);
+            this.enableVoiceButton.Name = "enableVoiceButton";
+            this.enableVoiceButton.Size = new System.Drawing.Size(221, 36);
+            this.enableVoiceButton.TabIndex = 1;
+            this.enableVoiceButton.Text = "Enable Voice Control";
+            this.enableVoiceButton.UseVisualStyleBackColor = true;
+            this.enableVoiceButton.Click += new System.EventHandler(this.enableVoiceButton_Click);
+            // 
+            // disableVoiceButton
+            // 
+            this.disableVoiceButton.Enabled = false;
+            this.disableVoiceButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disableVoiceButton.Location = new System.Drawing.Point(251, 291);
+            this.disableVoiceButton.Name = "disableVoiceButton";
+            this.disableVoiceButton.Size = new System.Drawing.Size(218, 36);
+            this.disableVoiceButton.TabIndex = 2;
+            this.disableVoiceButton.Text = "Disable Voice Control";
+            this.disableVoiceButton.UseVisualStyleBackColor = true;
+            this.disableVoiceButton.Click += new System.EventHandler(this.disableVoiceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 335);
+            this.Controls.Add(this.disableVoiceButton);
+            this.Controls.Add(this.enableVoiceButton);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LazyVoice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button enableVoiceButton;
+        private System.Windows.Forms.Button disableVoiceButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Runtime.InteropServices;
+using WindowsInput;
+//using System.Speech.Synthesis; /// used to talk back
+namespace LazyVoiceV2
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+IntPtr wParam, IntPtr lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+
+            commands.Add(new string[] { "turn up", "turn down", "volume up", "volume down", "mute", "unmute" });
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(commands);
+            Grammar g = new Grammar(gb);
+
+
+            sre.LoadGrammarAsync(g); // keeps running all the time on different thread
+            sre.SetInputToDefaultAudioDevice();
+            sre.SpeechRecognized += Sre_SpeechRecognized;
+
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "volume up":
+                case "turn up":
+                    outputTextBox.Text += "Volume Up Heard\n";            
+                    volumeUp();
+
+
+                    break;
+                case "volume down":
+                case "turn down":
+                    outputTextBox.Text += "Volume Down Heard\n";
+                    volumeDown();
+                    break;
+                case "mute":
+                    outputTextBox.Text += "Muted \n";
+                    volumeDown();
+                    mute();
+                    break;
+
+                case "unmute":
+                    volumeUp();
+                    volumeDown();
+                    break;
+
+
+
+
+
+
+
+                default:
+                    outputTextBox.Text += e.Result.Text + " command not available. \n";
+                    break;
+            }
+        }
+
+        private void enableVoiceButton_Click(object sender, EventArgs e)
+        {
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+            disableVoiceButton.Enabled = true;
+        }
+
+        private void disableVoiceButton_Click(object sender, EventArgs e)
+        {
+            sre.RecognizeAsyncStop();
+            disableVoiceButton.Enabled = false;
+        }
+
+        private void volumeUp()
+        {
+            for (int i = 0; i < 13; i++)
+            {
+                InputSimulator.SimulateKeyPress(VirtualKeyCode.VOLUME_UP);
+            }
+        }
+
+        private void volumeDown()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                InputSimulator.SimulateKeyPress(VirtualKeyCode.VOLUME_DOWN);
+            }
+        }
+
+        private void mute()
+        {
+            InputSimulator.SimulateKeyPress(VirtualKeyCode.VOLUME_MUTE);
+        }
+
+    }
+}
